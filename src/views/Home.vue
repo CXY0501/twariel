d6ad52361e0c332ccc43704c8100e9c32af6a304
CXY0501 <chen.xy.2@foxmail.com>
2021-06-07 23:11:33 +0800

    20210607-03

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -35,13 +35,36 @@
       />
       <img src="../assets/img/ariel_PC_tw_05.jpeg" alt="" />
     </picture>
-    <picture class="picture">
-      <source
-        media="(max-width: 767px)"
-        srcset="../assets/img/ariel_MB_tw_06.jpeg"
-      />
-      <img src="../assets/img/ariel_PC_tw_06.jpeg" alt="" />
-    </picture>
+    <div class="buy">
+      <a
+        href="https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8312062&str_category_code=3900400629"
+        target="_blank"
+        id="momo_btn_1"
+      ></a>
+      <a
+        href="https://24h.pchome.com.tw/prod/DAAK7P-A900B12NO"
+        target="_blank"
+        id="pchome_btn_1"
+      ></a>
+      <a
+        href="https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8313566&str_category_code=3900400629&ctype=B&Area=DgrpCategory"
+        target="_blank"
+        id="momo_btn_2"
+      ></a>
+      <a
+        href="https://24h.pchome.com.tw/prod/DAAK7P-A900B1AAW?fq=/S/DAAK7P"
+        target="_blank"
+        id="pchome_btn_2"
+      ></a>
+      <picture class="picture">
+        <source
+          media="(max-width: 767px)"
+          srcset="../assets/img/ariel_MB_tw_06.jpeg"
+        />
+        <img src="../assets/img/ariel_PC_tw_06.jpeg" alt="" />
+      </picture>
+    </div>
+
     <picture class="picture">
       <source
         media="(max-width: 767px)"
@@ -63,13 +86,36 @@
       />
       <img src="../assets/img/ariel_PC_tw_09.jpeg" alt="" />
     </picture>
-    <picture class="picture">
-      <source
-        media="(max-width: 767px)"
-        srcset="../assets/img/ariel_MB_tw_10.jpeg"
-      />
-      <img src="../assets/img/ariel_PC_tw_10.jpeg" alt="" />
-    </picture>
+    <div class="buy">
+      <a
+        href="https://www.momoshop.com.tw/goods/GoodsDetail.jsp?i_code=8313568&str_category_code=3900400629"
+        target="_blank"
+        id="momo_btn_3"
+      ></a>
+      <a
+        href="https://24h.pchome.com.tw/prod/DAAK7P-A900B12HX"
+        target="_blank"
+        id="pchome_btn_3"
+      ></a>
+      <a
+        href="https://www.momoshop.com.tw/category/DgrpCategory.jsp?d_code=3900400629&p_orderType=6&showType=chessboardType"
+        target="_blank"
+        id="momo_btn_4"
+      ></a>
+      <a
+        href="https://24h.pchome.com.tw/store/DAAK7P"
+        target="_blank"
+        id="pchome_btn_4"
+      ></a>
+      <picture class="picture">
+        <source
+          media="(max-width: 767px)"
+          srcset="../assets/img/ariel_MB_tw_10.jpeg"
+        />
+        <img src="../assets/img/ariel_PC_tw_10.jpeg" alt="" />
+      </picture>
+    </div>
+
     <picture class="picture">
       <source
         media="(max-width: 767px)"
@@ -106,7 +152,39 @@
       <img src="../assets/img/ariel_PC_tw_15.jpeg" alt="" />
     </picture>
     <footer id="float">
-      <img src="../assets/img/ariel_PC_tw_btn.png" alt="" />
+      <div class="father">
+        <div class="buttons">
+          <li class="footerlink">
+            <a
+              href="https://www.momoshop.com.tw/category/DgrpCategory.jsp?d_code=3900400629&p_orderType=4&showType=chessboardType"
+              target="_blank"
+              id="momo_btn_5"
+            ></a>
+          </li>
+          <li class="footerlink">
+            <a
+              href="https://24h.pchome.com.tw/store/DAAK7P"
+              target="_blank"
+              id="pchome_btn_5"
+            ></a>
+          </li>
+          <li class="footerlink">
+            <a
+              href="https://tw.buy.yahoo.com/search/product?p=ariel%E6%B4%97%E8%A1%A3"
+              target="_blank"
+              id="yahoo_btn_1"
+            ></a>
+          </li>
+          <li class="footerlink">
+            <a
+              href="https://www.books.com.tw/web/sys_brand/0/0000015648/0/"
+              target="_blank"
+              id="books_btn_1"
+            ></a>
+          </li>
+        </div>
+        <img src="../assets/img/ariel_PC_tw_btn.png" alt="" />
+      </div>
     </footer>
   </div>
 </template>
@@ -243,6 +321,34 @@ footer {
 
 footer img {
   width: 70%;
+  display: block;
+}
+
+.father {
+  postion: relative;
+}
+.buttons {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  right: 2%;
+  top: 37%;
+}
+footer li {
+  display: inline-block;
+  /* background-color: lightblue; */
+  /* margin-top: 100px; */
+  margin-left: 0.5%;
+  margin-bottom: 10px;
+  height: 50%;
+  width: 16%;
+  z-index: 999;
+}
+
+footer li a {
+  display: block;
+  width: 100%;
+  height: 100%;
 }
 
 @media screen and (max-width: 767px) {
@@ -257,6 +363,33 @@ footer img {
 
   footer img {
     width: 100%;
+  }
+
+  .father {
+    postion: relative;
+  }
+  .buttons {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    right: 2.5%;
+    top: 37%;
+  }
+  footer li {
+    display: inline-block;
+    /* background-color: lightblue; */
+    /* margin-top: 100px; */
+    margin-left: 0.7%;
+    margin-bottom: 10px;
+    height: 50%;
+    width: 23%;
+    z-index: 999;
+  }
+
+  footer li a {
+    display: block;
+    width: 100%;
+    height: 100%;
   }
 
   #momo_btn_1 {
